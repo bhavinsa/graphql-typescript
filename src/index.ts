a6376e06f81b4869ff98a9eddbdab1ad80c70cfe
bhavinsa <bhavinpatelsa@gmail.com>
2019-06-22 16:47:11 +0530

    config port added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { GraphQLServer } from "graphql-yoga";
 import "reflect-metadata";
 import { buildSchema, Query } from "type-graphql";
+import config = require("config");
 import ProjectResolver from "./resolvers/ProjectResolver";
 import TaskResolver from "./resolvers/TaskResolver";
 import EmployeeResolver from "./resolvers/EmployeeResolver";
 import CompanyResolver from "./resolvers/CompanyResolver";
+
 async function bootstrap() {
   const schema = await buildSchema({
     resolvers: [
@@ -18,9 +20,9 @@ async function bootstrap() {
   const server = new GraphQLServer({
     schema
   });
-
+  const port: number = config.get("application.app.port");
   const options = {
-    port: 4000,
+    port: port,
     // endpoint: "/graphql",
     // subscriptions: "/subscriptions",
     playground: "/"
